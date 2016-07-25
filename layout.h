c6847b509f68716b2c1d905b27606579e1341c3e
cancel <cancel@cancel.fm>
2016-07-25 17:42:22 +0900

    Add comments in .h file for API functions

--- layout.h
+++ layout.h
@@ -225,47 +225,131 @@ LAY_STATIC_INLINE lay_vec4 lay_vec4_xyzw(lay_scalar x, lay_scalar y, lay_scalar 
 #endif
 }
 
+// Call this on a context before using it. You must also call this on a context
+// if you would like to use it again after calling lay_destroy_context() on it.
 LAY_EXPORT void lay_init_context(lay_context *ctx);
+
+// Reserve enough heap memory to contain `count` items without needing to
+// reallocate. The initial lay_init_context() call does not allocate any heap
+// memory, so if you init a context and then call this once with a large enough
+// number for the number of items you'll create, there will not be any further
+// reallocations.
 LAY_EXPORT void lay_reserve_items_capacity(lay_context *ctx, lay_id count);
+
+// Frees any heap allocated memory used by a context. Don't call this on a
+// context that did not have lay_init_context() call on it. To reuse a context
+// after destroying it, you will need to call lay_init_context() on it again.
 LAY_EXPORT void lay_destroy_context(lay_context *ctx);
 
+// Clears all of the items in a context, setting its count to 0. Use this when
+// you want to re-declare your layout starting from the root item. This does not
+// free any memory or perform allocations. It's safe to use the context again
+// after calling this. You should probably use this instead of init/destroy if
+// you are recalculating your layouts in a loop.
 LAY_EXPORT void lay_reset_context(lay_context *ctx);
+
+// Performs the layout calculations, starting at the root item (id 0). After
+// calling this, you can use lay_get_rect() to query for an item's calculated
+// rectangle. If you use procedures such as lay_append() or lay_insert() after
+// calling this, your calculated data may become invalid if a reallocation
+// occurs.
+//
+// You should prefer to recreate your items starting from the root instead of
+// doing fine-grained updates to the existing context.
+//
+// However, it's safe to use lay_set_size on an item, and then re-run
+// lay_run_context. This might be useful if you are doing a resizing animation
+// on items in a layout without any contents changing.
 LAY_EXPORT void lay_run_context(lay_context *ctx);
 
+// Returns the number of items that have been created in a context.
 LAY_EXPORT lay_id lay_items_count(lay_context *ctx);
+
+// Returns the number of items the context can hold without performing a
+// reallocation.
 LAY_EXPORT lay_id lay_items_capacity(lay_context *ctx);
 
+// Create a new item, which can just be thought of as a rectangle. Returns the
+// id (handle) used to identify the item.
 LAY_EXPORT lay_id lay_item(lay_context *ctx);
-LAY_EXPORT void lay_append(lay_context *ctx, lay_id earlier, lay_id later);
+
+// Inserts an item into another item, forming a parent - child relationship. An
+// item can contain any number of child items. Items inserted into a parent are
+// put at the end of the ordering, after any existing siblings.
 LAY_EXPORT void lay_insert(lay_context *ctx, lay_id parent, lay_id child);
+
+// lay_append inserts an item as a sibling after another item. This allows
+// inserting an item into the middle of an existing list of items within a
+// parent. It's also more efficient than repeatedly using lay_insert(ctx,
+// parent, new_child) in a loop to create a list of items in a parent, because
+// it does not need to traverse the parent's children each time. So if you're
+// creating a long list of children inside of a parent, you might prefer to use
+// this after using lay_insert to insert the first child.
+LAY_EXPORT void lay_append(lay_context *ctx, lay_id earlier, lay_id later);
+
+// Like lay_insert, but puts the new item as the first child in a parent instead
+// of as the last.
 LAY_EXPORT void lay_push(lay_context *ctx, lay_id parent, lay_id child);
+
+// Gets the size that was set with lay_set_size or lay_set_size_xy.
 LAY_EXPORT lay_vec2 lay_get_size(lay_context *ctx, lay_id item);
+
+// Sets the size of an item. The _xy version passes the width and height as
+// separate arguments, but functions the same.
 LAY_EXPORT void lay_set_size(lay_context *ctx, lay_id item, lay_vec2 size);
 LAY_EXPORT void lay_set_size_xy(lay_context *ctx, lay_id item, lay_scalar width, lay_scalar height);
+
+// Set the flags on an item which determines how it behaves as a parent. For
+// example, setting LAY_COLUMN will make an item behave as if it were a column
+// -- it will lay out its children vertically.
 LAY_EXPORT void lay_set_contain(lay_context *ctx, lay_id item, uint32_t flags);
+
+// Set the flags on an item which determines how it behaves as a child inside of
+// a parent item. For example, setting LAY_VFILL will make an item try to fill
+// up all available vertical space inside of its parent.
 LAY_EXPORT void lay_set_behave(lay_context *ctx, lay_id item, uint32_t flags);
+
+// Get the margins that were set by lay_set_margins.
 LAY_EXPORT lay_vec4 lay_get_margins(lay_context *ctx, lay_id item);
+
+// Set the margins on an item. The components of the vector are:
+// 0: left, 1: top, 2: right, 3: bottom.
 LAY_EXPORT void lay_set_margins(lay_context *ctx, lay_id item, lay_vec4 ltrb);
+
+// Same as lay_set_margins, but the components are passed as separate arguments
+// (left, top, right, bottom).
 LAY_EXPORT void lay_set_margins_ltrb(lay_context *ctx, lay_id item, lay_scalar l, lay_scalar t, lay_scalar r, lay_scalar b);
 
+// Get the pointer to an item in the buffer by its id. Don't keep this around --
+// it will become invalid as soon as any reallocation occurs. Just store the id
+// instead (it's smaller, anyway, and the lookup cost will be nothing.)
 LAY_STATIC_INLINE lay_item_t *lay_get_item(const lay_context *ctx, lay_id id)
 {
     LAY_ASSERT(id != LAY_INVALID_ID && id < ctx->count);
     return ctx->items + id;
 }
 
+// Get the id of first child of an item, if any. Returns LAY_INVALID_ID if there
+// is no child.
 LAY_STATIC_INLINE lay_id lay_first_child(const lay_context *ctx, lay_id id)
 {
     const lay_item_t *pitem = lay_get_item(ctx, id);
     return pitem->first_child;
 }
 
+// Get the id of the next sibling of an item, if any. Returns LAY_INVALID_ID if
+// there is no next sibling.
 LAY_STATIC_INLINE lay_id lay_next_sibling(const lay_context *ctx, lay_id id)
 {
     const lay_item_t *pitem = lay_get_item(ctx, id);
     return pitem->next_sibling;
 }
 
+// Returns the calculated rectangle of an item. This is only valid after calling
+// lay_run_context and before any other reallocation occurs. Otherwise, the
+// result will be undefined. The vector components are:
+// 0: x starting position, 1: y starting position
+// 2: width, 3: height
 LAY_STATIC_INLINE lay_vec4 lay_get_rect(const lay_context *ctx, lay_id id)
 {
     LAY_ASSERT(id != LAY_INVALID_ID && id < ctx->count);
